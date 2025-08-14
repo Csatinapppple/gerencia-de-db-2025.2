90712a6b18fa4511002e968d727011e61fa55a58
cuds <ana@r.c>
2025-08-14 23:11:44 +0200

    exercicio 2 aula 2

--- aula2/exercicio/40_exercicios.sql
+++ aula2/exercicio/40_exercicios.sql
@@ -1,66 +1,86 @@
 --40 Exercícios Resolvidos em MySQL - Completo
 --SEQUÊNCIA DE 40 EXERCÍCIOS RESOLVIDOS EM MYSQL
 ---SCRIPT DE PREPARAÇÃO (Schema + Dados)
+-- SCRIPT ALTERADO PARA FUNCIONAR EM SQLITE
+-- não e necessario essas chamadas de database ja que o database 
+-- no sqlite é somente um arquivo
+/*
 DROP DATABASE IF EXISTS loja;
 CREATE DATABASE loja CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE loja;
+*/
 -- Tabelas principais
+/*
+No SQLite não se tem a tipagem varchar, se usando somente o TEXT para especificar texto
+mas todas declaracoes de varchar sao automaticamente convertidas para TEXT então não
+precisa alterar todas essas chamadas
+ */
+
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE categorias (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(80) NOT NULL UNIQUE
+	id INTEGER PRIMARY KEY,
+	nome TEXT NOT NULL UNIQUE
 );
+
 CREATE TABLE produtos (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(120) NOT NULL,
-	preco DECIMAL(10,2) NOT NULL CHECK (preco >= 0),
-	estoque INT NOT NULL DEFAULT 0 CHECK (estoque >= 0),
-	categoria_id INT,
-	criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	atualizado_em DATETIME NULL,
+	id INTEGER PRIMARY KEY ,
+	nome TEXT NOT NULL,
+	preco REAL NOT NULL CHECK (preco >= 0),
+	estoque INTEGER NOT NULL DEFAULT 0 CHECK (estoque >= 0),
+	categoria_id INTEGER,
+	criado_em TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	atualizado_em TEXT NULL,
 	CONSTRAINT fk_prod_cat FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
+
 CREATE TABLE clientes (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(120) NOT NULL,
-	email VARCHAR(120) NOT NULL UNIQUE,
-	cidade VARCHAR(80),
-	estado CHAR(2),
-	criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+	id INTEGER PRIMARY KEY ,
+	nome TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	cidade TEXT,
+	estado TEXT CHECK(LENGTH(estado) = 2),
+	criado_em TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE pedidos (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	cliente_id INT NOT NULL,
-	data_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	status ENUM('novo','pago','enviado','cancelado') NOT NULL DEFAULT 'novo',
+	id INTEGER PRIMARY KEY ,
+	cliente_id INTEGER NOT NULL,
+	data_pedido TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	status TEXT NOT NULL DEFAULT 'novo' CHECK(status IN ('novo', 'pago', 'enviado', 'cancelado')),
 	CONSTRAINT fk_ped_cli FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
+
 CREATE TABLE pedido_itens (
-	pedido_id INT NOT NULL,
-	produto_id INT NOT NULL,
-	qtd INT NOT NULL CHECK (qtd > 0),
-	preco_unit DECIMAL(10,2) NOT NULL CHECK (preco_unit >= 0),
+	pedido_id INTEGER NOT NULL,
+	produto_id INTEGER NOT NULL,
+	qtd INTEGER NOT NULL CHECK (qtd > 0),
+	preco_unit REAL NOT NULL CHECK (preco_unit >= 0),
 	PRIMARY KEY (pedido_id, produto_id),
 	CONSTRAINT fk_pi_ped FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
 	CONSTRAINT fk_pi_prod FOREIGN KEY (produto_id) REFERENCES produtos(id)
-	40 Exercícios Resolvidos em MySQL - Completo
 );
+
 CREATE TABLE fornecedores (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(120) NOT NULL UNIQUE,
-	cnpj CHAR(14) NOT NULL UNIQUE
+	id INTEGER PRIMARY KEY,
+	nome TEXT NOT NULL UNIQUE,
+	cnpj TEXT NOT NULL UNIQUE CHECK(LENGTH(cnpj) = 14)
 );
+
 CREATE TABLE movimentos_estoque (
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-	produto_id INT NOT NULL,
-	tipo ENUM('entrada','saida') NOT NULL,
-	quantidade INT NOT NULL CHECK (quantidade > 0),
-	motivo VARCHAR(120),
-	criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	id INTEGER PRIMARY KEY ,
+	produto_id INTEGER NOT NULL,
+	tipo TEXT NOT NULL CHECK( tipo IN ('entrada', 'saida')),
+	quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+	motivo TEXT,
+	criado_em TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT fk_mov_prod FOREIGN KEY (produto_id) REFERENCES produtos(id)
 );
 -- Dados iniciais
+
 INSERT INTO categorias (nome) VALUES
 ('Eletrônicos'), ('Livros'), ('Games'), ('Papelaria');
+
 INSERT INTO produtos (nome, preco, estoque, categoria_id) VALUES
 ('Mouse Óptico', 49.90, 50, 1),
 ('Teclado Mecânico', 299.00, 20, 1),
@@ -68,26 +88,30 @@ INSERT INTO produtos (nome, preco, estoque, categoria_id) VALUES
 ('Livro SQL', 89.90, 100, 2),
 ('Caneta Gel', 4.50, 500, 4),
 ('Jogo Aventura', 199.90, 15, 3);
+
 INSERT INTO clientes (nome, email, cidade, estado) VALUES
 ('Ana Lima', 'ana@ex.com', 'São Paulo', 'SP'),
 ('Bruno Dias', 'bruno@ex.com', 'Rio de Janeiro', 'RJ'),
 ('Carla Nunes', 'carla@ex.com', 'Belo Horizonte', 'MG');
+
 INSERT INTO fornecedores (nome, cnpj) VALUES
 ('Tech Distribuidora', '12345678000199'),
 ('Livraria Central', '99887766000111');
+
 INSERT INTO pedidos (cliente_id, status, data_pedido) VALUES
 (1, 'pago', '2025-08-01 10:00:00'),
 (2, 'enviado', '2025-08-02 14:30:00'),
 (1, 'novo', '2025-08-05 09:15:00'),
 (3, 'cancelado','2025-08-06 16:00:00');
+
 INSERT INTO pedido_itens (pedido_id, produto_id, qtd, preco_unit) VALUES
 (1, 1, 2, 49.90),
 (1, 4, 1, 89.90),
 (2, 2, 1, 299.00),
 (2, 6, 1, 199.90),
-40 Exercícios Resolvidos em MySQL - Completo
 (3, 3, 1, 499.00),
 (4, 5, 10, 4.50);
+
 INSERT INTO movimentos_estoque (produto_id, tipo, quantidade, motivo) VALUES
 (1, 'entrada', 20, 'Compra fornecedor'),
 (1, 'saida', 5, 'Ajuste'),
@@ -95,11 +119,90 @@ INSERT INTO movimentos_estoque (produto_id, tipo, quantidade, motivo) VALUES
 ---
 --EXERCÍCIOS E SOLUÇÕES
 --1) Criar uma tabela de auditoria simples.
---CREATE TABLE auditoria_precos (...);
+
+CREATE TABLE auditoria_precos (
+    id INTEGER PRIMARY KEY,
+    produto_id INTEGER NOT NULL,
+    preco_antigo REAL NOT NULL,
+    preco_novo REAL NOT NULL,
+    data_alteracao TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
+CREATE TRIGGER tr_auditoria_preco
+AFTER UPDATE OF preco ON produtos
+FOR EACH ROW
+WHEN OLD.preco != NEW.preco
+BEGIN
+	
+	UPDATE produtos SET atualizado_em = CURRENT_TIMESTAMP WHERE id = OLD.id;
+
+	INSERT INTO auditoria_precos (
+		produto_id,
+		preco_antigo,
+		preco_novo,
+		data_alteracao
+	)
+	VALUES (
+		OLD.id,
+		OLD.preco,
+		NEW.preco,
+		CURRENT_TIMESTAMP
+	);
+END;
+
+-- testar nova tabela
+
+UPDATE produtos SET preco = 39.99 WHERE nome = 'SSD 1TB';
+
 --2) Adicionar coluna `sku` única em `produtos`.
 --ALTER TABLE produtos ADD COLUMN sku VARCHAR(30) UNIQUE AFTER nome;
+/*
+	O SQLite nao consegue adicionar colunas depois de uma coluna especifica
+	somente consegue adicionar no final, também tem o problema de ser especificada como
+	UNIQUE, ja que o valor default vai ser NULL para todas as linhas de entrada então ao
+	adicionar para uma tabela com multiplas entradas já vai se encontrar um erro logico ja
+	que automaticamente vai se popular todas com NULL
+ */
+-- resposta que funciona com sqlite requer criar uma tabela nova e popular ela com os dados antigos
+
+ALTER TABLE produtos
+ADD COLUMN sku TEXT;
+	
+UPDATE produtos
+SET sku = 'SKU-' || id WHERE sku IS NULL;
+
+BEGIN TRANSACTION;
+
+PRAGMA foreign_keys = OFF;
+
+CREATE TABLE produtos_temp (
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    sku TEXT UNIQUE,
+    preco REAL NOT NULL CHECK (preco >= 0),
+    estoque INTEGER NOT NULL DEFAULT 0 CHECK (estoque >= 0),
+    categoria_id INTEGER,
+    criado_em TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TEXT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+INSERT INTO produtos_temp
+(id, nome, sku, preco, estoque, categoria_id, criado_em, atualizado_em)
+SELECT id, nome, sku, preco, estoque, categoria_id, criado_em, atualizado_em  FROM produtos;
+
+DROP TABLE produtos;
+ALTER TABLE produtos_temp RENAME TO produtos;
+
+PRAGMA foreign_keys = ON;
+COMMIT;
+
 --3) Tornar `cidade` NOT NULL e definir default.
 --ALTER TABLE clientes MODIFY cidade VARCHAR(80) NOT NULL DEFAULT 'Indefinida';
+
+
+
 --4) Criar índice composto em `pedido_itens` (por produto e qtd).
 --CREATE INDEX ix_pi_prod_qtd ON pedido_itens (produto_id, qtd);
 --5) Remover índice criado no exercício anterior.
